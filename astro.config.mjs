2983b789c02359a95cd2b0402f757224516157a3
Mordechai Dror <vorant94@gmail.com>
2023-02-11 22:11:03 +0200

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://photo-inbox.github.io',
+    integrations: [tailwind()]
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: true,
+	theme: {
+		colors: {
+			primary: "#ff7601",
+			secondary: "#fecc01",
+			black: "#242424",
+			white: "white",
+		},
+		fontFamily: {
+			"pt-serif": ["PT Serif", "serif"],
+			montserrat: ["Montserrat", "sans-serif"],
+		},
+		extend: {
+			backgroundImage: {
+				underline: "url('./assets/icons/underline.svg')"
+			},
+			keyframes: {
+				"fade-in-down": {
+					"0%": {
+						opacity: "0",
+						transform: "translateY(-10px)",
+					},
+					"100%": {
+						opacity: "1",
+						transform: "translateY(0)",
+					},
+				},
+			},
+			animation: {
+				"fade-in-down": "fade-in-down 0.5s ease-out",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
